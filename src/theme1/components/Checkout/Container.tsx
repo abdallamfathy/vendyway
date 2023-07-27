dfee401e69f944828ffa23a32312ec936c19323f
abdallamfathy <abdallamfathy@gmail.com>
2023-07-27 16:36:39 +0300

    make py only md device checkout page

--- src/theme1/components/Checkout/Container.tsx
+++ src/theme1/components/Checkout/Container.tsx
@@ -38,7 +38,7 @@ const Container: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto md:my-16 py-8 p-4">
+    <div className="container mx-auto md:my-16 max-md:py-8 max-md:p-4">
             <div className="md:flex hidden md:flex-row flex-col gap-6 justify-between items-start">
                 <div className="flex flex-col gap-[3.9rem] md:w-7/12 w-full checkoutShadow">
                 <Delivery/>
